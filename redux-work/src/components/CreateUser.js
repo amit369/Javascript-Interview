f99c06f9c57f70a473ff5ee4863fce5b14add030
amit369 <amitchaman369@gmail.com>
2023-02-19 11:05:02 +0530

    change committed

---
+++ redux-work/src/components/CreateUser.js
@@ -0,0 +1,42 @@
+import React , {useState} from "react";
+
+const CreateUser = () => {
+
+    const [name,setName] = useState();
+    const [email,setEmail] = useState();
+    const [password,setPassword] = useState();
+    const [address, setAddress] = useState();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const data = {
+            id : new Date(),
+            name ,
+            email,
+            password,
+            address
+        }
+        console.log(data);
+    }
+  return (
+    <div>
+        <form onSubmit={handleSubmit}>
+      <label for="name">Name</label>
+      <input type="text" value={name} onChange={ (e) => { setName(e.target.value)}}/>
+      <br />
+      <label for="name">Email</label>
+      <input type="text" value={email} onChange={ (e) => { setEmail(e.target.value)}}/>
+      <br />
+      <label for="name">Password</label>
+      <input type="text" value={password} onChange={ (e) => { setPassword(e.target.value)}}/>
+      <br />
+      <label for="name">Address</label>
+      <input type="text" value={address} onChange={ (e) => { setAddress(e.target.value)}}/>
+       <br/>
+      <button type="submit">Submit </button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateUser;
